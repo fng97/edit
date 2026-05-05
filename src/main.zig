eb01c82d86230b8779d39b701b9a9e54cae41cca
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-05-05 14:46:58 +0100

    Replace std.posix.read usage with std.Io.Reader

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,4 @@
-//                                 ASCII TABLE
-//                                 ===========
-//
+// ASCII cheatsheet:
 //      0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
 // 0x0  NUL  SOH  STX  ETX  EOT  ENQ  ACK  BEL  BS   HT   LF   VT   FF   CR   SO   SI
 // 0x1  DLE  DC1  DC2  DC3  DC4  NAK  SYN  ETB  CAN  EM   SUB  ESC  FS   GS   RS   US
@@ -17,7 +15,10 @@ const assert = std.debug.assert;
 var termios_original: ?std.posix.termios = null;
 pub fn panic_after_termios_restore(msg: []const u8, first_trace_addr: ?usize) noreturn {
     @branchHint(.cold);
-    if (termios_original) |t| std.posix.tcsetattr(std.Io.File.stdin().handle, .FLUSH, t) catch {};
+    if (termios_original) |t| { // only do this once
+        std.posix.tcsetattr(std.Io.File.stdin().handle, .FLUSH, t) catch {};
+        termios_original = null;
+    }
     std.debug.defaultPanic(msg, first_trace_addr);
 }
 pub const panic = std.debug.FullPanic(panic_after_termios_restore);
@@ -27,8 +28,12 @@ pub fn main(init: std.process.Init) !void {
 
     const stdin = std.Io.File.stdin();
     var stdin_buffer: [128]u8 = undefined; // TODO: What's a reasonable size here?
+    var stdin_reader = stdin.reader(io, &stdin_buffer);
+    const reader: *std.Io.Reader = &stdin_reader.interface;
+
     // TODO: Later, we'll want to buffer the whole screen and flush in one go. Will need to work out
-    // the maximum buffer size based on resolution and rendering.
+    // the maximum buffer size based on resolution and rendering (have to account for escape
+    // sequences).
     const stdout = std.Io.File.stdout();
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = stdout.writer(io, &stdout_buffer);
@@ -61,9 +66,8 @@ pub fn main(init: std.process.Init) !void {
     try writer.writeAll("\x1b[?u"); // CSI ? u <- query KKP flags
     try writer.writeAll("\x1b[c"); // CSI c <- query device attributes
     try writer.flush(); // send KKP init and queries to stdout
-    const read_size = try std.posix.read(stdin.handle, &stdin_buffer);
-    // Assert KKP mode 1 enabled: CSI ? 1 u.
-    if (!std.mem.startsWith(u8, stdin_buffer[0..read_size], "\x1b[?1u"))
+    // Assert KKP mode 1 enabled: CSI ? 1 u (5 bytes).
+    if (!std.mem.eql(u8, try reader.take(5), "\x1b[?1u"))
         return error.KittyKeyboardProtocolNotSupported;
     defer {
         writer.writeAll("\x1b[<u") catch {}; // pop KKP flags
@@ -71,17 +75,13 @@ pub fn main(init: std.process.Init) !void {
         writer.writeAll("\x1b[H") catch {}; // place cursor at top left
         writer.flush() catch {};
     }
-    // Make sure DA1 was consumed. Read again if not yet received.
-    var da1 = stdin_buffer[5..read_size]; // KKP response was 5 bytes
-    if (da1.len == 0) {
-        const da1_size = try std.posix.read(stdin.handle, &stdin_buffer);
-        da1 = stdin_buffer[0..da1_size];
-    }
-    // DA1 starts with CSI ? and ends with c.
-    if (!std.mem.startsWith(u8, da1, "\x1b[?") or !std.mem.endsWith(u8, da1, "c"))
-        return error.InvalidDa1Response;
+    // Make sure DA1 was consumed. DA1 starts with CSI ? and ends with c (only c in sequence).
+    if (!std.mem.eql(u8, try reader.take(3), "\x1b[?")) return error.MissingDA1Response;
+    _ = try reader.discardDelimiterInclusive('c');
 
     // Get window size.
+    // TODO: Query it with CSI 18 t instead of doing ioctl.
+    // try writer.writeAll("\x1b[18t"); // query window size
     var winsize: std.posix.winsize = .{ .row = 0, .col = 0, .xpixel = 0, .ypixel = 0 };
     const err = std.posix.system.ioctl(stdout.handle, std.posix.T.IOCGWINSZ, @intFromPtr(&winsize));
     assert(std.posix.errno(err) == .SUCCESS);
@@ -94,9 +94,5 @@ pub fn main(init: std.process.Init) !void {
     try writer.writeAll("\x1b[H"); // place cursor at top left
     try writer.flush();
 
-    event_loop: while (true) {
-        const size = try std.posix.read(stdin.handle, &stdin_buffer);
-        const bytes = stdin_buffer[0..size];
-        for (bytes) |byte| if (byte == 'q') break :event_loop;
-    }
+    while (true) if (try reader.takeByte() == 'q') break;
 }
