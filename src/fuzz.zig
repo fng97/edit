a6ba03fedccf394fbb8d58f46b0017254a7726d0
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-07-31 11:19:18 +0100

    Generate inputs in fuzz test

--- src/fuzz.zig
+++ src/fuzz.zig
@@ -14,6 +14,14 @@ pub fn main(init: std.process.Init) !void {
 
     var frng: FRNG = .{ .entropy = entropy };
 
+    // Generate file name.
+    // TODO: Is this big enough? Make it look more like a path?
+    const file_name_size = frng.logInt(u8) catch return;
+    const file_name_buffer = try allocator.alloc(u8, file_name_size);
+    defer allocator.free(file_name_buffer);
+    for (file_name_buffer) |*byte| byte.* = frng.rangeInclusive(u8, 0x20, 0x7E) catch return;
+
+    // Generate file contents.
     // TODO: Handle opening empty files: add a single newline rather than supporting empty files.
     const file_size = frng.logRangeInclusive(u32, 1, @intCast(frng.entropy.len)) catch return;
     const file_buffer = try allocator.alloc(u8, file_size);
@@ -32,24 +40,38 @@ pub fn main(init: std.process.Init) !void {
     };
     file_buffer[file_buffer.len - 1] = '\n';
 
-    // TODO: Is this big enough? Make it look more like a path?
-    const file_name_size = frng.logInt(u8) catch return;
-    const file_name_buffer = try allocator.alloc(u8, file_name_size);
-    defer allocator.free(file_name_buffer);
-    for (file_name_buffer) |*byte| byte.* = frng.rangeInclusive(u8, 0x20, 0x7E) catch return;
-
+    // Generate viewport dimensions.
     const row_count = frng.logInt(u16) catch return;
     const col_count = frng.logInt(u16) catch return;
-    const resize = try std.fmt.allocPrint(
-        allocator,
-        "\x1b[48;{d};{d};0;0t", // pix values ignored
-        .{ row_count, col_count },
-    );
-    defer allocator.free(resize);
-    var reader = std.Io.Reader.fixed(resize);
+
+    // Use the remaining entropy to generate inputs.
+    var input: std.Io.Writer.Allocating = .init(allocator);
+    defer input.deinit();
+    // First input must be resize (parsed during init below for dimensions).
+    try input.writer.print("\x1b[48;{d};{d};0;0t", .{ row_count, col_count }); // pix values ignored
+    while (true) switch (frng.weighted(.{ .move = 900, .resize = 10, .quit = 1 }) catch break) {
+        .move => try input.writer.writeByte(switch (frng.weighted(.{
+            .h = 1,
+            .j = 1,
+            .k = 1,
+            .l = 1,
+        }) catch break) {
+            .h => 'h',
+            .j => 'j',
+            .k => 'k',
+            .l => 'l',
+        }),
+        .resize => try input.writer.print("\x1b[48;{d};{d};0;0t", .{
+            frng.logInt(u16) catch break,
+            frng.logInt(u16) catch break,
+        }),
+        .quit => break,
+    };
+    try input.writer.writeByte('q'); // guarantee a clean exit
+    var reader: std.Io.Reader = .fixed(input.written());
+
     var writer = std.Io.Writer.Allocating.init(allocator);
     defer writer.deinit();
-
     var editor = Editor.init(
         allocator,
         &reader,
@@ -63,5 +85,5 @@ pub fn main(init: std.process.Init) !void {
     };
     defer editor.deinit();
 
-    // TODO: Feed input to stdin. Call tick instead. Make render private again.
+    while (try editor.tick()) {}
 }
